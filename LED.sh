6b4971868b74849da2564a50bd50b609d8ffc11e
Simon Jourdenais <symonak18@gmail.com>
2019-11-19 09:12:30 +0000

    Final Commit - Termine LED.sh

--- LED.sh
+++ LED.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
-# Initialisation des GPIOs. Les GPIOs nécessaires sont activés avec la commande export, puis la direction est choisie.
-echo "71" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio71/direction
-
-echo "233" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio233/direction
-
-while : 
-do
-if [ $(cat /sys/class/gpio/gpio233/value) -eq 0 ];
-	then 
-		echo "1" > /sys/class/gpio/gpio71/value
-	else
-		echo "0" > /sys/class/gpio/gpio71/value
+DIR71="/sys/class/gpio/gpio71"
+DIR233="/sys/class/gpio/gpio233"
+if [ ! -d "$DIR71" ];
+	then
+		echo "71" > /sys/class/gpio/export
+		echo "out" > /sys/class/gpio/gpio71/direction
 fi
-done
+if [ ! -d "$DIR233" ];
+	then
+		echo "233" > /sys/class/gpio/export
+		echo "out" > /sys/class/gpio/gpio233/direction
+		while :
+		do
+ 		if [ $(cat /sys/class/gpio/gpio233/value) -eq 0 ];
+			then
+				echo "1" > /sys/class/gpio/gpio233/value
+			else
+				echo "0" > /sys/class/gpio/gpio233/value
+		fi
+		done
+elif [ -d "$DIR233" ];
+	then
+		while :
+		do
+		if [ $(cat /sys/class/gpio/gpio233/value) -eq 0 ];
+			then
+				echo "1" > /sys/class/gpio/gpio233/value
+			else 
+				echo "0" > /sys/class/gpio/gpio233/value
+		fi
+		done
+fi
